dcacfe5a3859cf08df8ab0fa21513654685c7762
Vignesh <vignesh1999@gmail.com>
2020-09-13 23:34:53 +0530

    added auto join for meet w selenium

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # google-meet-auto-join
 
+Only works in Python2 and Google Chrome.
+
+Calendar events must have attached Google Meet link or Zoom link as description of event.
+
 ## Enable google calendar api
 
 Follow the steps in this link to enable Google Calendar API - https://developers.google.com/calendar/quickstart/python
 
 Copy credentials.json into the directory where this repo has been cloned.
 
-Only works in Python2 and calendar events must have attached Google meet link or Zoom link as description of event.
-
 ## Install dependencies
 
 ```pip install -r dependencies.txt```
+
+Download required chromedriver version and move it to the working directory.
+
+Before running script, make sure Gmail account details have been added in auto-open.py lines 159 and 160.
 
 ## Running the script
 
--- auto-open.py
+++ auto-open.py
@@ -5,6 +5,12 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from time import sleep
 import webbrowser
 import time
 import pytz
@@ -15,7 +21,49 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 SLEEP_WINDOW_SECS = 60 * 30
 OPEN_MEETING_MINUTES_BEFORE = 0
 speech_engine = pyttsx3.init()
-speech_engine.setProperty('rate',150)
+speech_engine.setProperty('rate', 165)
+
+class Join:
+    def __init__(self, platform, username, password, url):
+        opt = Options()
+        opt.add_argument("--disable-infobars")
+        opt.add_argument("start-maximized")
+        opt.add_argument("--disable-extensions")
+
+        #giving permission to chrome
+        opt.add_experimental_option("prefs", { \
+            "profile.default_content_setting_values.media_stream_mic": 1, 
+            "profile.default_content_setting_values.media_stream_camera": 1, 
+            "profile.default_content_setting_values.notifications": 1 
+        })
+        if platform == 'Meet':
+            #logging into google through stack overflow
+            self.driver = webdriver.Chrome(chrome_options=opt, executable_path='./chromedriver')
+            self.driver.get('https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%2fstory%2fcurrent%27')
+            sleep(3)
+            self.driver.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click()
+            self.driver.find_element_by_xpath('//input[@type="email"]').send_keys(username)
+            self.driver.find_element_by_xpath('//*[@id="identifierNext"]').click()
+            sleep(3)
+            self.driver.find_element_by_xpath('//input[@type="password"]').send_keys(password)
+            self.driver.find_element_by_xpath('//*[@id="passwordNext"]').click()
+            sleep(2)
+            self.driver.get(url)
+            sleep(4)
+            elem = self.driver.find_element_by_css_selector('body')
+            #muting audio
+            elem.send_keys(Keys.CONTROL + 'd')
+            sleep(1)
+            #muting video
+            elem.send_keys(Keys.CONTROL + 'e')
+            sleep(1)
+            #joining
+            self.driver.find_element_by_xpath('//*[@class="XCoPyb"]/div[1]').click()
+        elif platform == 'Zoom':
+            self.driver = webdriver.Chrome(chrome_options=opt, executable_path='./chromedriver')
+            self.driver.get(url)
+            sleep(10)
+            speech_engine.say("Manual operation required.")
 
 
 def get_creds():
@@ -36,6 +84,26 @@ def get_creds():
             pickle.dump(creds, token)
     return creds
 
+""" #ONLY FOR TESTING
+def add_event(start_time, end_time):
+    creds = get_creds()
+    service = build('calendar', 'v3', credentials=creds)
+
+    event = {
+        'summary': 'TestMeetingNew',
+        'description': 'meet.google.com/urk-sgmr-cwy',
+        'start': {
+            'dateTime': start_time,
+            'timeZone': 'Asia/Kolkata',
+        },
+        'end': {
+            'dateTime': end_time,
+            'timeZone': 'Asia/Kolkata',
+        }
+    }
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print ('Event created: %s' % (event.get('htmlLink')))"""
+
 def get_meeting_info():
     creds = get_creds()
     service = build('calendar', 'v3', credentials=creds)
@@ -43,34 +111,39 @@ def get_meeting_info():
     #Call the Calendar API and get now time
     now = datetime.datetime.utcnow().isoformat() + 'Z' 
     #Setting time window for 8 hours after which app will stop running
-    now_plus_window = (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).isoformat() + 'Z'
+    now_plus_window = (datetime.datetime.utcnow() + datetime.timedelta(hours=24)).isoformat() + 'Z'
     print('\nGetting the upcoming events\n')
     events_result = service.events().list(calendarId='primary', timeMin=now, timeMax=now_plus_window, maxResults=5, singleEvents=True, orderBy='startTime').execute()
     events = events_result.get('items', [])
 
-    meet_link = ""
+    meet_link = ''
+    platform = ''
     for event in events:
         start_time = datetime.datetime.strptime(event['start'].get('dateTime', event['start'].get('date')), '%Y-%m-%dT%H:%M:%S+05:30')
         print(start_time, event['summary'])
 
         #checking if its cloud computing class lol
-        if 'Babukarthik' in event['summary']:
+        if 'Babukarthik' in event['summary'] or 'TestMeetingNew' in event['summary']:
             meet_link = 'https://' + event['description'].split()[-1]
+            platform = 'Meet'
             print("Google Meet Link(Babu): ", meet_link)
 
         #checking if Google meet link is attached
         if 'conferenceData' in event:
             if 'meet' in event['conferenceData'].get('entryPoints')[0].get('uri'):
                 meet_link = event['conferenceData'].get('entryPoints')[0].get('uri')
+                platform = 'Meet'
                 print("Google Meet Link(Others): ", meet_link)
         #checking if meeting is on Zoom
         elif 'description' in event and 'zoom' in event['description']:
             meet_link = event['description']
+            platform = 'Zoom'
             print("Zoom Link: ", meet_link)
         
         if meet_link and is_time_in_future(start_time):
             return {
                 'meet_link': meet_link,
+                'platform': platform,
                 'start_time': start_time,
                 'meeting_name': event.get('summary')
             }
@@ -79,9 +152,13 @@ def get_meeting_info():
 def is_time_in_future(meet_start_time):
     return meet_start_time > datetime.datetime.now()
 
-def open_meeting_in_browser(meet_link):
+def open_meeting_in_browser(meet_link, platform):
     print('Initiating meeting at:', meet_link)
-    webbrowser.open(meet_link, new=1)
+
+    #Add Gmail ID and password before running
+    username = ''
+    password = ''
+    Join(platform, username, password, meet_link)
 
 def get_time_till_next_meeting(meeting_start_time):
     if meeting_start_time:
@@ -91,7 +168,7 @@ def get_time_till_next_meeting(meeting_start_time):
 
 def alert_on_meeting():
     #text to speech function to alert
-    alert_message = "Meeting will start now! Click on join now!"
+    alert_message = "Meeting will start now!"
     speech_engine.say(alert_message)
     speech_engine.runAndWait()
 
@@ -101,13 +178,16 @@ def main():
     secs_till_next_meeting = SLEEP_WINDOW_SECS + 10
     meeting_name = ''
 
+    """ #ONLY FOR TESTING
+    add_event((datetime.datetime.now() + datetime.timedelta(seconds=40)).isoformat(), (datetime.datetime.now() + datetime.timedelta(minutes=5)).isoformat())"""
+
     while True:
         # showing the next meeting
         if meeting_should_be_shown_soon and meeting_link:
-            open_meeting_in_browser(meeting_link)
+            alert_on_meeting()
+            open_meeting_in_browser(meeting_link, meeting_platform)
             meeting_should_be_shown_soon = False
             meeting_link = None
-            alert_on_meeting()
             meeting_name = ''
         # getting the next meeting
         else:
@@ -116,6 +196,7 @@ def main():
                 meeting_link = meeting_details.get('meet_link')
                 meeting_start_time = meeting_details.get('start_time')
                 meeting_name = meeting_details.get('meeting_name')
+                meeting_platform = meeting_details.get('platform')
                 secs_till_next_meeting = get_time_till_next_meeting(meeting_start_time)
             else:
                 print ("No upcoming meetings in the given window.\nExiting application.")
